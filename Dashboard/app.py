89d71d371b24bd3a5a76d2799fecf1e5e1758d99
rohithkumar-bhat <rohithkumar@entrolabs.com>
2026-03-25 16:58:58 +0530

    Update app.py

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -67,4 +67,4 @@ app.mount("/static", StaticFiles(directory="."), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app")
